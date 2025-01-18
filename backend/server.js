239535545c311562ba0e59da80976ddb006d4f2f
Kevinkaidao <kevinkaidao@gmail.com>
2025-01-18 13:56:09 -0500

    Added Routes and Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,25 @@
 require('dotenv').config()
+
+const express = require('express')
 const mongoose = require('mongoose')
+
+// Express App
+const app = express()
+
+app.use((req, res, next) => {
+    console.log("MIDDLEWARE: " + req.path, req.method)
+    next()
+})
+
+// Routes
+app.get('/', (req, res) => {
+    res.json({ message : "Welcome to the app!" })
+})
+
+// Listen for Requests
+app.listen(process.env.PORT, () => {
+    console.log("Listening on port: " + process.env.PORT)
+})
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
@@ -8,3 +28,4 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((error) => {
         console.log(error)
     })
+
