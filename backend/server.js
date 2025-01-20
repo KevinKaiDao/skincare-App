14a61ffe3a1fe977c983f5b59be38c59aec1a2bc
Kevinkaidao <kevinkaidao@gmail.com>
2025-01-20 11:16:31 -0500

    Rearranged and neated up code.

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const productRoutes = require('./routes/products')
 
 // Express App
 const app = express()
-const productRoutes = require('./routes/products')
 
+// Middleware
+app.use(express.json())
 app.use((req, res, next) => {
     console.log("MIDDLEWARE: " + req.path, req.method)
     next()
@@ -15,14 +17,12 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/products/', productRoutes)
 
-// Listen for Requests
-app.listen(process.env.PORT, () => {
-    console.log("Listening on port: " + process.env.PORT)
-})
-
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        console.log("Connected to MongoDB!")
+        // Listen for Requests
+        app.listen(process.env.PORT, () => {
+            console.log("Listening on port: " + process.env.PORT)
+        })
     })
     .catch((error) => {
         console.log(error)
