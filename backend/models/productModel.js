f35fc5ec1b6e91b952d38a4b7b4917b0a05bd2a1
Kevin Dao <Kevinkaidao@gmail.com>
2025-01-20 17:10:35 -0500

    Updated productModel schema.

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -2,13 +2,25 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const productSchema = new Schema({
+const productSchema = new Schema({ // TO-DO: Add ingredients list
     name: {
-        type: String,
+        type: String, 
         required: true
     },
-    type: {
-        type: String,
+    brand: {
+        type: String, // Innisfree, TheOrdinary, Isntree, etc etc
         required: true
+    },
+    category: {
+        type: String, // Serums, Mositurizers, Cleanser, Toner, etc etc
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    imageURL: {
+        type: String,
+        required: false
     }
 })
